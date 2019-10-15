7204bd4833666b402b2a075b27f263dcd90a6298
YogiHa <hagaiharari1@gmail.com>
2019-10-15 18:21:01 +0300

    lesson 4

--- src/components/CreateTodo/CreateTodo.js
+++ src/components/CreateTodo/CreateTodo.js
@@ -7,15 +7,22 @@ export default function CreateTodo() {
 
     const handleSubmit = e => {
         e.preventDefault();
+        console.log(todo)
     }
 
     return (
         <Grid container spacing={1} style={{ background: '#2E3B55'}}>
                <Grid item xs={9}>
-                <TextField fullWidth variant="outlined" placeholder="add todo" />
+               <form name="contact-form" onSubmit={handleSubmit}>
+                <TextField
+                 fullWidth
+                  variant="outlined"
+                   placeholder="add todo"
+                   onChange={e=>setTodo(e.target.value)} />
+                </form>
                 </Grid>
                <Grid item xs={3} style={{ background: '#4a724a' }}>
-               <Button>
+               <Button onClick={handleSubmit}>
                add
                </Button>
                </Grid>
